bd731888a8c42960b832ad03a2449305c086c2a4
unknown <carlosestacio1@gmail.com>
2023-10-23 11:09:47 -0300

    estados

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { useState } from 'react'
+
+
 function App() {
+  const [valor, setValor] = useState('');
   return (
     <div>
-
+      <input type="text" placeholder='digite aqui' onChange={({target}) => setValor(target.value)} />
+      <span>{valor}</span>
     </div>
   )
 
